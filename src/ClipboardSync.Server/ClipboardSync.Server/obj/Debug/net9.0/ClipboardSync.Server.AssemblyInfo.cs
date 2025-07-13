25011a70ee91aec9f116e3e6c83927cb528c9607
yuri <buryy137@gmail.com>
2025-07-13 13:27:43 +0300

    recreate structure projects

--- src/ClipboardSync.Server/ClipboardSync.Server/obj/Debug/net9.0/ClipboardSync.Server.AssemblyInfo.cs
+++ src/ClipboardSync.Server/ClipboardSync.Server/obj/Debug/net9.0/ClipboardSync.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClipboardSync.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aa0d8c5f719bca06ec3b107e2ce518abf03c341")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b575258a820f9950768a102310fc4555eb62ad4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClipboardSync.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClipboardSync.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
